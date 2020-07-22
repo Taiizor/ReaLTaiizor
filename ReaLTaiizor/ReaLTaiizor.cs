c0c48c652412582cbde8ff79842722fdedeaf2e0
Taiizor <galata.80@hotmail.com>
2020-07-22 04:46:59 +0300

    53X Again 130!

--- ReaLTaiizor/ReaLTaiizor.cs
+++ ReaLTaiizor/ReaLTaiizor.cs
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 //     Creator: Taiizor
 //     Site   : www.Taiizor.com
 //     Created: 15.May.2019
-//     Changed: 21.Jul.2020
+//     Changed: 22.Jul.2020
 //     Version: 3.7.7.5
 //
 // |---------DO-NOT-REMOVE---------|
