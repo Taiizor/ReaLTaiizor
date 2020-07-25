2696b6c8297b8db453ca32563061aa927867c583
Taiizor <galata.80@hotmail.com>
2020-07-25 20:28:14 +0300

    53X Again 131!

--- ReaLTaiizor/obj/Release/net461/ReaLTaiizor.AssemblyInfo.cs
+++ ReaLTaiizor/obj/Release/net461/ReaLTaiizor.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © 2020 Taiizor Inc.")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("C# WinForm UI/UX Library - ReaLTaiizor")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("3.7.7.5")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("3.7.7.5+41601a3a9121787dd5be3d7f0d635a4d6ca8e837")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("3.7.7.5+c0c48c652412582cbde8ff79842722fdedeaf2e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReaLTaiizor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReaLTaiizor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("3.7.7.5")]
