f7246a30a4385c3b90d0cb50942c3e3f318ef31a
Taiizor <galata.80@hotmail.com>
2020-07-26 19:02:36 +0300

    53X Again 148!

--- ReaLTaiizor.Avira/Avira.Designer.cs
+++ ReaLTaiizor.Avira/Avira.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Avira));
+            this.SuspendLayout();
+            // 
+            // Avira
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Avira";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Avira";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
