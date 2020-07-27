dfff1774f297e4f0b064b120029cb975ec832521
Taiizor <galata.80@hotmail.com>
2020-07-27 17:15:04 +0300

    53X Again 160!

---
+++ demo/ReaLTaiizor.UX/ReaLTaiizor.UX/Main.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace ReaLTaiizor.UX
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
