7e262fdbb7991d43841749c91550ce012073168e
Taiizor <41683699+Taiizor@users.noreply.github.com>
2020-08-19 20:47:02 +0300

    53X Again 203!

--- src/ReaLTaiizor_CR/Ribbon.Designer.cs
+++ src/ReaLTaiizor_CR/Ribbon.Designer.cs
@@ -34,10 +34,21 @@
             // ribbonForm1
             // 
             this.ribbonForm1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.ribbonForm1.BaseColor = System.Drawing.Color.Fuchsia;
             this.ribbonForm1.BottomLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(99)))), ((int)(((byte)(99)))));
+            this.ribbonForm1.CompositingQualityType = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
             this.ribbonForm1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ribbonForm1.HatchType = System.Drawing.Drawing2D.HatchStyle.ForwardDiagonal;
+            this.ribbonForm1.HeaderLineColorA = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.ribbonForm1.HeaderLineColorB = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.ribbonForm1.HeaderLineColorC = System.Drawing.Color.Black;
             this.ribbonForm1.Location = new System.Drawing.Point(0, 0);
             this.ribbonForm1.Name = "ribbonForm1";
+            this.ribbonForm1.RibbonEdgeColorA = System.Drawing.Color.Black;
+            this.ribbonForm1.RibbonEdgeColorB = System.Drawing.Color.Black;
+            this.ribbonForm1.RibbonEdgeColorC = System.Drawing.Color.Black;
+            this.ribbonForm1.RibbonEdgeColorD = System.Drawing.Color.FromArgb(((int)(((byte)(86)))), ((int)(((byte)(86)))), ((int)(((byte)(86)))));
+            this.ribbonForm1.RibbonEdgeColorE = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(50)))));
             this.ribbonForm1.Size = new System.Drawing.Size(800, 450);
             this.ribbonForm1.SubTitle = "Ribbon";
             this.ribbonForm1.SubTitleColor = System.Drawing.Color.WhiteSmoke;
