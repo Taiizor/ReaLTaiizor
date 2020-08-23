aef250ea82631d8dcb057f2552e4279f4e91df4c
Taiizor <41683699+Taiizor@users.noreply.github.com>
2020-08-23 23:51:31 +0300

    53X Again 252!

--- src/ReaLTaiizor_CR/Material.Designer.cs
+++ src/ReaLTaiizor_CR/Material.Designer.cs
@@ -30,6 +30,12 @@
         {
             this.materialTextBox1 = new ReaLTaiizor.MaterialTextBox();
             this.materialTextBox2 = new ReaLTaiizor.MaterialTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.materialRadioButton1 = new ReaLTaiizor.MaterialRadioButton();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // materialTextBox1
@@ -38,7 +44,7 @@
             this.materialTextBox1.Depth = 0;
             this.materialTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.materialTextBox1.Hint = "53X";
-            this.materialTextBox1.Location = new System.Drawing.Point(42, 87);
+            this.materialTextBox1.Location = new System.Drawing.Point(12, 85);
             this.materialTextBox1.MaxLength = 50;
             this.materialTextBox1.MouseState = ReaLTaiizor.MaterialDrawHelper.MaterialMouseState.OUT;
             this.materialTextBox1.Multiline = false;
@@ -53,7 +59,7 @@
             this.materialTextBox2.Depth = 0;
             this.materialTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.materialTextBox2.Hint = "53X";
-            this.materialTextBox2.Location = new System.Drawing.Point(42, 143);
+            this.materialTextBox2.Location = new System.Drawing.Point(12, 141);
             this.materialTextBox2.MaxLength = 50;
             this.materialTextBox2.MouseState = ReaLTaiizor.MaterialDrawHelper.MaterialMouseState.OUT;
             this.materialTextBox2.Multiline = false;
@@ -63,11 +69,78 @@
             this.materialTextBox2.TabIndex = 1;
             this.materialTextBox2.Text = "deneme";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 197);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "deneme";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 223);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(259, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "deneme";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 249);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.PasswordChar = '●';
+            this.textBox3.Size = new System.Drawing.Size(259, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "deneme";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 275);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.PasswordChar = '●';
+            this.textBox4.Size = new System.Drawing.Size(259, 20);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "deneme";
+            // 
+            // materialRadioButton1
+            // 
+            this.materialRadioButton1.AutoSize = true;
+            this.materialRadioButton1.Depth = 0;
+            this.materialRadioButton1.Location = new System.Drawing.Point(349, 154);
+            this.materialRadioButton1.Margin = new System.Windows.Forms.Padding(0);
+            this.materialRadioButton1.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.materialRadioButton1.MouseState = ReaLTaiizor.MaterialDrawHelper.MaterialMouseState.HOVER;
+            this.materialRadioButton1.Name = "materialRadioButton1";
+            this.materialRadioButton1.Ripple = true;
+            this.materialRadioButton1.Size = new System.Drawing.Size(190, 37);
+            this.materialRadioButton1.TabIndex = 6;
+            this.materialRadioButton1.TabStop = true;
+            this.materialRadioButton1.Text = "materialRadioButton1";
+            this.materialRadioButton1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 301);
+            this.richTextBox1.Multiline = false;
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(259, 55);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "deneme";
+            // 
             // Material
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(340, 215);
+            this.ClientSize = new System.Drawing.Size(736, 376);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.materialRadioButton1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.materialTextBox2);
             this.Controls.Add(this.materialTextBox1);
             this.MaximizeBox = false;
@@ -75,6 +148,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Material";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -82,5 +156,11 @@
 
         private ReaLTaiizor.MaterialTextBox materialTextBox1;
         private ReaLTaiizor.MaterialTextBox materialTextBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private ReaLTaiizor.MaterialRadioButton materialRadioButton1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
--- src/ReaLTaiizor_CR/Material.cs
+++ src/ReaLTaiizor_CR/Material.cs
@@ -7,6 +7,11 @@ namespace ReaLTaiizor_CR
         public Material()
         {
             InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new MaterialColorScheme(MaterialPrimary.BlueGrey800, MaterialPrimary.BlueGrey900, MaterialPrimary.BlueGrey500, MaterialAccent.LightBlue200, MaterialTextShade.WHITE);
         }
     }
 }
