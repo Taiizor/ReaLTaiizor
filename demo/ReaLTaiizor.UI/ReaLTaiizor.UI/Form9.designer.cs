c09d16c73f2e4125ad1a839ea78abcfa315e48ba
Taiizor <41683699+Taiizor@users.noreply.github.com>
2020-08-25 19:03:37 +0300

    53X Again 307!

--- demo/ReaLTaiizor.UI/ReaLTaiizor.UI/Form9.designer.cs
+++ demo/ReaLTaiizor.UI/ReaLTaiizor.UI/Form9.designer.cs
@@ -29,179 +29,179 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form9));
-            skyStatusBar1 = new ReaLTaiizor.SkyStatusBar();
-            skyNumeric1 = new ReaLTaiizor.SkyNumeric();
-            skyLabel1 = new ReaLTaiizor.SkyLabel();
-            skyTextBox1 = new ReaLTaiizor.SkyTextBox();
-            skyComboBox1 = new ReaLTaiizor.SkyComboBox();
-            skyCheckBox1 = new ReaLTaiizor.SkyCheckBox();
-            skyRadioButton2 = new ReaLTaiizor.SkyRadioButton();
-            skyRadioButton1 = new ReaLTaiizor.SkyRadioButton();
-            skyButton1 = new ReaLTaiizor.SkyButton();
-            SuspendLayout();
+            this.skyStatusBar1 = new ReaLTaiizor.Controls.StatusBar.SkyStatusBar();
+            this.skyNumeric1 = new ReaLTaiizor.Controls.Numeric.SkyNumeric();
+            this.skyLabel1 = new ReaLTaiizor.Controls.Label.SkyLabel();
+            this.skyTextBox1 = new ReaLTaiizor.Controls.TextBox.SkyTextBox();
+            this.skyComboBox1 = new ReaLTaiizor.Controls.ComboBox.SkyComboBox();
+            this.skyCheckBox1 = new ReaLTaiizor.Controls.CheckBox.SkyCheckBox();
+            this.skyRadioButton2 = new ReaLTaiizor.Controls.RadioButton.SkyRadioButton();
+            this.skyRadioButton1 = new ReaLTaiizor.Controls.RadioButton.SkyRadioButton();
+            this.skyButton1 = new ReaLTaiizor.Controls.Button.SkyButton();
+            this.SuspendLayout();
             // 
             // skyStatusBar1
             // 
-            skyStatusBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            skyStatusBar1.Font = new System.Drawing.Font("Verdana", 6.75F, System.Drawing.FontStyle.Bold);
-            skyStatusBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(94)))), ((int)(((byte)(137)))));
-            skyStatusBar1.Location = new System.Drawing.Point(0, 107);
-            skyStatusBar1.Name = "skyStatusBar1";
-            skyStatusBar1.Size = new System.Drawing.Size(263, 23);
-            skyStatusBar1.TabIndex = 8;
-            skyStatusBar1.Text = "skyStatusBar1";
+            this.skyStatusBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.skyStatusBar1.Font = new System.Drawing.Font("Verdana", 6.75F, System.Drawing.FontStyle.Bold);
+            this.skyStatusBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(94)))), ((int)(((byte)(137)))));
+            this.skyStatusBar1.Location = new System.Drawing.Point(0, 107);
+            this.skyStatusBar1.Name = "skyStatusBar1";
+            this.skyStatusBar1.Size = new System.Drawing.Size(263, 23);
+            this.skyStatusBar1.TabIndex = 8;
+            this.skyStatusBar1.Text = "skyStatusBar1";
             // 
             // skyNumeric1
             // 
-            skyNumeric1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(233)))), ((int)(((byte)(233)))));
-            skyNumeric1.Cursor = System.Windows.Forms.Cursors.IBeam;
-            skyNumeric1.Font = new System.Drawing.Font("Verdana", 6.75F, System.Drawing.FontStyle.Bold);
-            skyNumeric1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(94)))), ((int)(((byte)(137)))));
-            skyNumeric1.Location = new System.Drawing.Point(174, 65);
-            skyNumeric1.Maximum = ((long)(100));
-            skyNumeric1.Minimum = ((long)(0));
-            skyNumeric1.Name = "skyNumeric1";
-            skyNumeric1.Size = new System.Drawing.Size(75, 30);
-            skyNumeric1.TabIndex = 7;
-            skyNumeric1.Text = "skyNumeric1";
-            skyNumeric1.Value = ((long)(50));
+            this.skyNumeric1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(233)))), ((int)(((byte)(233)))));
+            this.skyNumeric1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.skyNumeric1.Font = new System.Drawing.Font("Verdana", 6.75F, System.Drawing.FontStyle.Bold);
+            this.skyNumeric1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(94)))), ((int)(((byte)(137)))));
+            this.skyNumeric1.Location = new System.Drawing.Point(174, 65);
+            this.skyNumeric1.Maximum = ((long)(100));
+            this.skyNumeric1.Minimum = ((long)(0));
+            this.skyNumeric1.Name = "skyNumeric1";
+            this.skyNumeric1.Size = new System.Drawing.Size(75, 30);
+            this.skyNumeric1.TabIndex = 7;
+            this.skyNumeric1.Text = "skyNumeric1";
+            this.skyNumeric1.Value = ((long)(50));
             // 
             // skyLabel1
             // 
-            skyLabel1.AutoSize = true;
-            skyLabel1.Font = new System.Drawing.Font("Verdana", 6.75F, System.Drawing.FontStyle.Bold);
-            skyLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(94)))), ((int)(((byte)(137)))));
-            skyLabel1.Location = new System.Drawing.Point(194, 41);
-            skyLabel1.Name = "skyLabel1";
-            skyLabel1.Size = new System.Drawing.Size(55, 12);
-            skyLabel1.TabIndex = 6;
-            skyLabel1.Text = "skyLabel1";
+            this.skyLabel1.AutoSize = true;
+            this.skyLabel1.Font = new System.Drawing.Font("Verdana", 6.75F, System.Drawing.FontStyle.Bold);
+            this.skyLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(94)))), ((int)(((byte)(137)))));
+            this.skyLabel1.Location = new System.Drawing.Point(194, 41);
+            this.skyLabel1.Name = "skyLabel1";
+            this.skyLabel1.Size = new System.Drawing.Size(55, 12);
+            this.skyLabel1.TabIndex = 6;
+            this.skyLabel1.Text = "skyLabel1";
             // 
             // skyTextBox1
             // 
-            skyTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(233)))), ((int)(((byte)(233)))));
-            skyTextBox1.Font = new System.Drawing.Font("Verdana", 6.75F, System.Drawing.FontStyle.Bold);
-            skyTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(94)))), ((int)(((byte)(137)))));
-            skyTextBox1.Location = new System.Drawing.Point(174, 12);
-            skyTextBox1.MaxLength = 32767;
-            skyTextBox1.MultiLine = false;
-            skyTextBox1.Name = "skyTextBox1";
-            skyTextBox1.Size = new System.Drawing.Size(75, 21);
-            skyTextBox1.TabIndex = 5;
-            skyTextBox1.Text = "skyTextBox1";
-            skyTextBox1.TextAlignment = System.Windows.Forms.HorizontalAlignment.Center;
-            skyTextBox1.UseSystemPasswordChar = false;
+            this.skyTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(233)))), ((int)(((byte)(233)))));
+            this.skyTextBox1.Font = new System.Drawing.Font("Verdana", 6.75F, System.Drawing.FontStyle.Bold);
+            this.skyTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(94)))), ((int)(((byte)(137)))));
+            this.skyTextBox1.Location = new System.Drawing.Point(174, 12);
+            this.skyTextBox1.MaxLength = 32767;
+            this.skyTextBox1.MultiLine = false;
+            this.skyTextBox1.Name = "skyTextBox1";
+            this.skyTextBox1.Size = new System.Drawing.Size(75, 21);
+            this.skyTextBox1.TabIndex = 5;
+            this.skyTextBox1.Text = "skyTextBox1";
+            this.skyTextBox1.TextAlignment = System.Windows.Forms.HorizontalAlignment.Center;
+            this.skyTextBox1.UseSystemPasswordChar = false;
             // 
             // skyComboBox1
             // 
-            skyComboBox1.BackColor = System.Drawing.Color.Transparent;
-            skyComboBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            skyComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
-            skyComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            skyComboBox1.Font = new System.Drawing.Font("Verdana", 6.75F, System.Drawing.FontStyle.Bold);
-            skyComboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(94)))), ((int)(((byte)(137)))));
-            skyComboBox1.FormattingEnabled = true;
-            skyComboBox1.ItemHeight = 16;
-            skyComboBox1.ItemHighlightColor = System.Drawing.Color.FromArgb(((int)(((byte)(121)))), ((int)(((byte)(176)))), ((int)(((byte)(214)))));
-            skyComboBox1.Items.AddRange(new object[] {
+            this.skyComboBox1.BackColor = System.Drawing.Color.Transparent;
+            this.skyComboBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.skyComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.skyComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.skyComboBox1.Font = new System.Drawing.Font("Verdana", 6.75F, System.Drawing.FontStyle.Bold);
+            this.skyComboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(94)))), ((int)(((byte)(137)))));
+            this.skyComboBox1.FormattingEnabled = true;
+            this.skyComboBox1.ItemHeight = 16;
+            this.skyComboBox1.ItemHighlightColor = System.Drawing.Color.FromArgb(((int)(((byte)(121)))), ((int)(((byte)(176)))), ((int)(((byte)(214)))));
+            this.skyComboBox1.Items.AddRange(new object[] {
             "Item 1",
             "Item 2",
             "Item 3",
             "Item 4",
             "Item 5"});
-            skyComboBox1.Location = new System.Drawing.Point(93, 12);
-            skyComboBox1.Name = "skyComboBox1";
-            skyComboBox1.Size = new System.Drawing.Size(75, 22);
-            skyComboBox1.StartIndex = 0;
-            skyComboBox1.TabIndex = 4;
+            this.skyComboBox1.Location = new System.Drawing.Point(93, 12);
+            this.skyComboBox1.Name = "skyComboBox1";
+            this.skyComboBox1.Size = new System.Drawing.Size(75, 22);
+            this.skyComboBox1.StartIndex = 0;
+            this.skyComboBox1.TabIndex = 4;
             // 
             // skyCheckBox1
             // 
-            skyCheckBox1.BackColor = System.Drawing.Color.Transparent;
-            skyCheckBox1.Checked = false;
-            skyCheckBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            skyCheckBox1.Font = new System.Drawing.Font("Verdana", 6.75F, System.Drawing.FontStyle.Bold);
-            skyCheckBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(94)))), ((int)(((byte)(137)))));
-            skyCheckBox1.Location = new System.Drawing.Point(12, 41);
-            skyCheckBox1.Name = "skyCheckBox1";
-            skyCheckBox1.Size = new System.Drawing.Size(90, 14);
-            skyCheckBox1.TabIndex = 3;
-            skyCheckBox1.Text = "skyCheckBox1";
+            this.skyCheckBox1.BackColor = System.Drawing.Color.Transparent;
+            this.skyCheckBox1.Checked = false;
+            this.skyCheckBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.skyCheckBox1.Font = new System.Drawing.Font("Verdana", 6.75F, System.Drawing.FontStyle.Bold);
+            this.skyCheckBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(94)))), ((int)(((byte)(137)))));
+            this.skyCheckBox1.Location = new System.Drawing.Point(12, 41);
+            this.skyCheckBox1.Name = "skyCheckBox1";
+            this.skyCheckBox1.Size = new System.Drawing.Size(90, 14);
+            this.skyCheckBox1.TabIndex = 3;
+            this.skyCheckBox1.Text = "skyCheckBox1";
             // 
             // skyRadioButton2
             // 
-            skyRadioButton2.BackColor = System.Drawing.Color.Transparent;
-            skyRadioButton2.Checked = true;
-            skyRadioButton2.Cursor = System.Windows.Forms.Cursors.Hand;
-            skyRadioButton2.Font = new System.Drawing.Font("Verdana", 6.75F, System.Drawing.FontStyle.Bold);
-            skyRadioButton2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(94)))), ((int)(((byte)(137)))));
-            skyRadioButton2.Location = new System.Drawing.Point(12, 81);
-            skyRadioButton2.Name = "skyRadioButton2";
-            skyRadioButton2.Size = new System.Drawing.Size(105, 14);
-            skyRadioButton2.TabIndex = 2;
-            skyRadioButton2.Text = "skyRadioButton2";
+            this.skyRadioButton2.BackColor = System.Drawing.Color.Transparent;
+            this.skyRadioButton2.Checked = true;
+            this.skyRadioButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.skyRadioButton2.Font = new System.Drawing.Font("Verdana", 6.75F, System.Drawing.FontStyle.Bold);
+            this.skyRadioButton2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(94)))), ((int)(((byte)(137)))));
+            this.skyRadioButton2.Location = new System.Drawing.Point(12, 81);
+            this.skyRadioButton2.Name = "skyRadioButton2";
+            this.skyRadioButton2.Size = new System.Drawing.Size(105, 14);
+            this.skyRadioButton2.TabIndex = 2;
+            this.skyRadioButton2.Text = "skyRadioButton2";
             // 
             // skyRadioButton1
             // 
-            skyRadioButton1.BackColor = System.Drawing.Color.Transparent;
-            skyRadioButton1.Checked = false;
-            skyRadioButton1.Cursor = System.Windows.Forms.Cursors.Hand;
-            skyRadioButton1.Font = new System.Drawing.Font("Verdana", 6.75F, System.Drawing.FontStyle.Bold);
-            skyRadioButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(94)))), ((int)(((byte)(137)))));
-            skyRadioButton1.Location = new System.Drawing.Point(12, 61);
-            skyRadioButton1.Name = "skyRadioButton1";
-            skyRadioButton1.Size = new System.Drawing.Size(105, 14);
-            skyRadioButton1.TabIndex = 1;
-            skyRadioButton1.Text = "skyRadioButton1";
+            this.skyRadioButton1.BackColor = System.Drawing.Color.Transparent;
+            this.skyRadioButton1.Checked = false;
+            this.skyRadioButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.skyRadioButton1.Font = new System.Drawing.Font("Verdana", 6.75F, System.Drawing.FontStyle.Bold);
+            this.skyRadioButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(94)))), ((int)(((byte)(137)))));
+            this.skyRadioButton1.Location = new System.Drawing.Point(12, 61);
+            this.skyRadioButton1.Name = "skyRadioButton1";
+            this.skyRadioButton1.Size = new System.Drawing.Size(105, 14);
+            this.skyRadioButton1.TabIndex = 1;
+            this.skyRadioButton1.Text = "skyRadioButton1";
             // 
             // skyButton1
             // 
-            skyButton1.BackColor = System.Drawing.Color.Transparent;
-            skyButton1.Cursor = System.Windows.Forms.Cursors.Hand;
-            skyButton1.Font = new System.Drawing.Font("Verdana", 6.75F, System.Drawing.FontStyle.Bold);
-            skyButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(94)))), ((int)(((byte)(137)))));
-            skyButton1.Location = new System.Drawing.Point(12, 12);
-            skyButton1.Name = "skyButton1";
-            skyButton1.Size = new System.Drawing.Size(75, 23);
-            skyButton1.TabIndex = 0;
-            skyButton1.Text = "skyButton1";
+            this.skyButton1.BackColor = System.Drawing.Color.Transparent;
+            this.skyButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.skyButton1.Font = new System.Drawing.Font("Verdana", 6.75F, System.Drawing.FontStyle.Bold);
+            this.skyButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(94)))), ((int)(((byte)(137)))));
+            this.skyButton1.Location = new System.Drawing.Point(12, 12);
+            this.skyButton1.Name = "skyButton1";
+            this.skyButton1.Size = new System.Drawing.Size(75, 23);
+            this.skyButton1.TabIndex = 0;
+            this.skyButton1.Text = "skyButton1";
             // 
             // Form9
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(263, 130);
-            Controls.Add(skyStatusBar1);
-            Controls.Add(skyNumeric1);
-            Controls.Add(skyLabel1);
-            Controls.Add(skyTextBox1);
-            Controls.Add(skyComboBox1);
-            Controls.Add(skyCheckBox1);
-            Controls.Add(skyRadioButton2);
-            Controls.Add(skyRadioButton1);
-            Controls.Add(skyButton1);
-            DoubleBuffered = true;
-            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            Icon = ((System.Drawing.Icon)(resources.GetObject("$Icon")));
-            MaximizeBox = false;
-            Name = "Form9";
-            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            Text = "Form9";
-            ResumeLayout(false);
-            PerformLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 130);
+            this.Controls.Add(this.skyStatusBar1);
+            this.Controls.Add(this.skyNumeric1);
+            this.Controls.Add(this.skyLabel1);
+            this.Controls.Add(this.skyTextBox1);
+            this.Controls.Add(this.skyComboBox1);
+            this.Controls.Add(this.skyCheckBox1);
+            this.Controls.Add(this.skyRadioButton2);
+            this.Controls.Add(this.skyRadioButton1);
+            this.Controls.Add(this.skyButton1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form9";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form9";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private ReaLTaiizor.SkyButton skyButton1;
-        private ReaLTaiizor.SkyRadioButton skyRadioButton1;
-        private ReaLTaiizor.SkyRadioButton skyRadioButton2;
-        private ReaLTaiizor.SkyCheckBox skyCheckBox1;
-        private ReaLTaiizor.SkyComboBox skyComboBox1;
-        private ReaLTaiizor.SkyTextBox skyTextBox1;
-        private ReaLTaiizor.SkyLabel skyLabel1;
-        private ReaLTaiizor.SkyNumeric skyNumeric1;
-        private ReaLTaiizor.SkyStatusBar skyStatusBar1;
+        private ReaLTaiizor.Controls.Button.SkyButton skyButton1;
+        private ReaLTaiizor.Controls.RadioButton.SkyRadioButton skyRadioButton1;
+        private ReaLTaiizor.Controls.RadioButton.SkyRadioButton skyRadioButton2;
+        private ReaLTaiizor.Controls.CheckBox.SkyCheckBox skyCheckBox1;
+        private ReaLTaiizor.Controls.ComboBox.SkyComboBox skyComboBox1;
+        private ReaLTaiizor.Controls.TextBox.SkyTextBox skyTextBox1;
+        private ReaLTaiizor.Controls.Label.SkyLabel skyLabel1;
+        private ReaLTaiizor.Controls.Numeric.SkyNumeric skyNumeric1;
+        private ReaLTaiizor.Controls.StatusBar.SkyStatusBar skyStatusBar1;
     }
 }
