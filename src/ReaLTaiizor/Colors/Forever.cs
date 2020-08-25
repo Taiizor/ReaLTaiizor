f6b5d7b32ff82f974b5065c41f4f949bc5b1aeac
Taiizor <41683699+Taiizor@users.noreply.github.com>
2020-08-25 05:53:42 +0300

    53X Again 284!

---
+++ src/ReaLTaiizor/Colors/Forever.cs
@@ -0,0 +1,18 @@
+﻿#region Imports
+
+using System.Drawing;
+using ReaLTaiizor.Utils;
+
+#endregion
+
+namespace ReaLTaiizor.Colors
+{
+    #region ForeverLibrary
+
+    public class ForeverColors
+    {
+        public Color Forever = ForeverLibrary.ForeverColor;
+    }
+
+    #endregion
+}
--- src/ReaLTaiizor/Controls/ComboBox/ForeverComboBox.cs
+++ src/ReaLTaiizor/Controls/ComboBox/ForeverComboBox.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Drawing;
+using ReaLTaiizor.Utils;
 using System.Drawing.Text;
 using System.Windows.Forms;
 using System.ComponentModel;
