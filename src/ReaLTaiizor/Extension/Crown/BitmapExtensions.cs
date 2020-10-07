9cafafdb31bb6c673c30ade59bbbfc5b0de29301
Taiizor <41683699+Taiizor@users.noreply.github.com>
2020-10-07 05:36:23 +0300

    53X Again 413!

---
+++ src/ReaLTaiizor/Extension/Crown/BitmapExtensions.cs
@@ -0,0 +1,45 @@
+﻿#region Imports
+
+using System.Drawing;
+
+#endregion
+
+namespace ReaLTaiizor.Extension.Crown
+{
+    #region BitmapExtensionsExtension
+
+    internal static class BitmapExtensions
+    {
+        internal static Bitmap SetColor(this Bitmap bitmap, Color color)
+        {
+            var newBitmap = new Bitmap(bitmap.Width, bitmap.Height);
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    var pixel = bitmap.GetPixel(i, j);
+                    if (pixel.A > 0)
+                        newBitmap.SetPixel(i, j, color);
+                }
+            }
+            return newBitmap;
+        }
+
+        internal static Bitmap ChangeColor(this Bitmap bitmap, Color oldColor, Color newColor)
+        {
+            var newBitmap = new Bitmap(bitmap.Width, bitmap.Height);
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    var pixel = bitmap.GetPixel(i, j);
+                    if (pixel == oldColor)
+                        newBitmap.SetPixel(i, j, newColor);
+                }
+            }
+            return newBitmap;
+        }
+    }
+
+    #endregion
+}
