5bcbdd57b56a6d80cf9919cf8f1735abad33697d
Taiizor <Taiizor@github.com>
2021-01-30 06:52:04 +0300

    53X Again 488!

--- src/ReaLTaiizor/ReaLTaiizor.cs
+++ src/ReaLTaiizor/ReaLTaiizor.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 //     Creator: Taiizor
 //     Website: www.Taiizor.com
 //     Created: 15.May.2019
-//     Changed: 14.Jan.2021
+//     Changed: 30.Jan.2021
 //     Version: 3.7.8.2
 //
 // |---------DO-NOT-REMOVE---------|
