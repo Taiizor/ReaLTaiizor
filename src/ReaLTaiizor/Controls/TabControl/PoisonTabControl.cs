6d20332836ec5641fe0dcc4e5de1a0044d814960
Taiizor <Taiizor@github.com>
2021-02-26 16:22:40 +0300

    53X Again 509!

--- src/ReaLTaiizor/Controls/TabControl/PoisonTabControl.cs
+++ src/ReaLTaiizor/Controls/TabControl/PoisonTabControl.cs
@@ -26,7 +26,9 @@ namespace ReaLTaiizor.Controls
 {
     #region PoisonTabControl
 
+#if !NET5_0 && !NET6_0
     [Designer(typeof(PoisonTabControlDesigner))]
+#endif
     [ToolboxBitmap(typeof(TabControl))]
     public class PoisonTabControl : TabControl, IPoisonControl
     {
