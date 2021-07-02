efb207cbe432f80b128374d73e298d02c5e01e83
Taiizor <Taiizor@github.com>
2021-07-02 10:13:58 +0300

    53X Again 613!

---
+++ src/ReaLTaiizor/Controls/Switch/ParrotSwitch.cs
@@ -0,0 +1,271 @@
+﻿#region Imports
+
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+#endregion
+
+namespace ReaLTaiizor.Controls
+{
+    #region ParrotSwitch
+
+    public class ParrotSwitch : Control
+    {
+        public ParrotSwitch()
+        {
+            base.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            BackColor = Color.Transparent;
+            base.Size = new Size(60, 30);
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("The text of the button")]
+        public Style SwitchStyle
+        {
+            get => switchStyle;
+            set
+            {
+                switchStyle = value;
+                SetSwitchColor = true;
+                if (value == Style.iOS)
+                {
+                    base.Size = new Size(60, 30);
+                }
+                if (value == Style.Android)
+                {
+                    base.Size = new Size(58, 30);
+                }
+                base.Invalidate();
+            }
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("The smoothing mode of the graphics")]
+        public State SwitchState
+        {
+            get => switchState;
+            set
+            {
+                switchState = value;
+                OnSwitchStateChanged();
+                base.Invalidate();
+            }
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("The button on color")]
+        public Color OnColor
+        {
+            get => onColor;
+            set
+            {
+                onColor = value;
+                SetSwitchColor = false;
+                base.Invalidate();
+            }
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("The button off color")]
+        public Color OffColor
+        {
+            get => offColor;
+            set
+            {
+                offColor = value;
+                SetSwitchColor = false;
+                base.Invalidate();
+            }
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("The button on color")]
+        public Color HandleOnColor
+        {
+            get => handleOnColor;
+            set
+            {
+                handleOnColor = value;
+                SetSwitchColor = false;
+                base.Invalidate();
+            }
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("The button off color")]
+        public Color HandleOffColor
+        {
+            get => handleOffColor;
+            set
+            {
+                handleOffColor = value;
+                SetSwitchColor = false;
+                base.Invalidate();
+            }
+        }
+
+        public event EventHandler SwitchStateChanged;
+
+        protected virtual void OnSwitchStateChanged()
+        {
+            SwitchStateChanged?.Invoke(this, new EventArgs());
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            if (SwitchState == State.On)
+            {
+                SwitchState = State.Off;
+                return;
+            }
+            SwitchState = State.On;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
+            if (switchStyle == Style.iOS)
+            {
+                if (SetSwitchColor)
+                {
+                    onColor = Color.FromArgb(76, 217, 100);
+                    handleOnColor = Color.FromArgb(255, 255, 255);
+                    offColor = Color.FromArgb(255, 255, 255);
+                    handleOffColor = Color.FromArgb(255, 255, 255);
+                }
+                if (switchState == State.On)
+                {
+                    e.Graphics.FillRectangle(new SolidBrush(onColor), 15, 0, 30, 29);
+                    e.Graphics.FillPie(new SolidBrush(onColor), new Rectangle(1, 0, 30, 29), 0f, 360f);
+                    e.Graphics.FillPie(new SolidBrush(onColor), new Rectangle(30, 0, 29, 29), 0f, 360f);
+                    e.Graphics.FillPie(new SolidBrush(handleOnColor), new Rectangle(31, 1, 27, 27), 0f, 360f);
+                    e.Graphics.FillPie(new SolidBrush(handleOnColor), new Rectangle(32, 2, 25, 25), 0f, 360f);
+                }
+                else
+                {
+                    e.Graphics.FillRectangle(new SolidBrush(offColor), 15, 0, 30, 29);
+                    e.Graphics.FillPie(new SolidBrush(offColor), new Rectangle(1, 0, 30, 29), 0f, 360f);
+                    e.Graphics.FillPie(new SolidBrush(offColor), new Rectangle(30, 0, 29, 29), 0f, 360f);
+                    e.Graphics.FillPie(new SolidBrush(Color.FromArgb(200, 200, 200)), new Rectangle(2, 1, 29, 27), 0f, 360f);
+                    e.Graphics.FillPie(new SolidBrush(handleOffColor), new Rectangle(3, 2, 27, 25), 0f, 360f);
+                }
+            }
+            if (switchStyle == Style.Android)
+            {
+                if (SetSwitchColor)
+                {
+                    onColor = Color.FromArgb(217, 239, 237);
+                    handleOnColor = Color.FromArgb(126, 199, 192);
+                    offColor = Color.FromArgb(77, 77, 77);
+                    handleOffColor = Color.FromArgb(185, 185, 185);
+                }
+                if (switchState == State.On)
+                {
+                    e.Graphics.FillRectangle(new SolidBrush(onColor), 10, 5, 30, 20);
+                    e.Graphics.FillPie(new SolidBrush(onColor), new Rectangle(3, 5, 20, 20), 0f, 360f);
+                    e.Graphics.FillPie(new SolidBrush(handleOnColor), new Rectangle(25, 0, 29, 29), 0f, 360f);
+                }
+                else
+                {
+                    e.Graphics.FillRectangle(new SolidBrush(offColor), 10, 5, 30, 20);
+                    e.Graphics.FillPie(new SolidBrush(offColor), new Rectangle(28, 5, 20, 20), 0f, 360f);
+                    e.Graphics.FillPie(new SolidBrush(handleOffColor), new Rectangle(0, 0, 29, 29), 0f, 360f);
+                }
+            }
+            if (switchStyle == Style.Horizontal)
+            {
+                if (switchState == State.On)
+                {
+                    e.Graphics.FillRectangle(new SolidBrush(onColor), 0, 5, base.Width, base.Height - 10);
+                    e.Graphics.FillRectangle(new SolidBrush(handleOnColor), base.Width / 2 + 2, 7, base.Width / 2 - 5, base.Height - 14);
+                }
+                else
+                {
+                    e.Graphics.FillRectangle(new SolidBrush(offColor), 0, 5, base.Width, base.Height - 10);
+                    e.Graphics.FillRectangle(new SolidBrush(handleOffColor), 2, 7, base.Width / 2 - 5, base.Height - 14);
+                }
+            }
+            if (switchStyle == Style.Vertical)
+            {
+                if (switchState == State.On)
+                {
+                    e.Graphics.FillRectangle(new SolidBrush(onColor), 5, 0, base.Width - 10, base.Height);
+                    e.Graphics.FillRectangle(new SolidBrush(handleOnColor), 7, base.Height / 2 + 2, base.Width - 14, base.Height / 2 - 5);
+                }
+                else
+                {
+                    e.Graphics.FillRectangle(new SolidBrush(offColor), 5, 0, base.Width - 10, base.Height);
+                    e.Graphics.FillRectangle(new SolidBrush(handleOffColor), 7, 2, base.Width - 14, base.Height / 2 - 5);
+                }
+            }
+            if (switchStyle == Style.Dark)
+            {
+                if (SetSwitchColor)
+                {
+                    onColor = Color.FromArgb(40, 40, 40);
+                    handleOnColor = Color.FromArgb(255, 255, 255);
+                    offColor = Color.FromArgb(75, 75, 75);
+                    handleOffColor = Color.FromArgb(255, 255, 255);
+                }
+                if (switchState == State.On)
+                {
+                    e.Graphics.FillRectangle(new SolidBrush(onColor), 15, 0, 30, 29);
+                    e.Graphics.FillPie(new SolidBrush(onColor), new Rectangle(1, 0, 30, 29), 0f, 360f);
+                    e.Graphics.FillPie(new SolidBrush(onColor), new Rectangle(30, 0, 29, 29), 0f, 360f);
+                    e.Graphics.FillPie(new SolidBrush(Color.FromArgb(200, 200, 200)), new Rectangle(31, 1, 27, 27), 0f, 360f);
+                    e.Graphics.FillPie(new SolidBrush(handleOnColor), new Rectangle(32, 2, 25, 25), 0f, 360f);
+                }
+                else
+                {
+                    e.Graphics.FillRectangle(new SolidBrush(offColor), 15, 0, 30, 29);
+                    e.Graphics.FillPie(new SolidBrush(offColor), new Rectangle(1, 0, 30, 29), 0f, 360f);
+                    e.Graphics.FillPie(new SolidBrush(offColor), new Rectangle(30, 0, 29, 29), 0f, 360f);
+                    e.Graphics.FillPie(new SolidBrush(Color.FromArgb(200, 200, 200)), new Rectangle(2, 1, 29, 27), 0f, 360f);
+                    e.Graphics.FillPie(new SolidBrush(handleOffColor), new Rectangle(3, 2, 27, 25), 0f, 360f);
+                }
+            }
+            base.OnPaint(e);
+        }
+
+        private bool SetSwitchColor = true;
+
+        private Style switchStyle = Style.Horizontal;
+
+        private State switchState;
+
+        private Color onColor = Color.FromArgb(102, 217, 174);
+
+        private Color offColor = Color.FromArgb(234, 129, 136);
+
+        private Color handleOnColor = Color.FromArgb(1, 180, 120);
+
+        private Color handleOffColor = Color.FromArgb(230, 71, 89);
+
+        public enum Style
+        {
+            Vertical,
+            Horizontal,
+            iOS,
+            Android,
+            Dark
+        }
+
+        public enum State
+        {
+            On,
+            Off
+        }
+    }
+
+    #endregion
+}
---
+++ src/ReaLTaiizor/Controls/Watcher/ParrotExtendedFileSystemWatcher.cs
@@ -0,0 +1,268 @@
+﻿#region Imports
+
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+using SysAction = System.Action;
+
+#endregion
+
+namespace ReaLTaiizor.Controls
+{
+    #region ParrotExtendedFileSystemWatcher
+
+    public class ParrotExtendedFileSystemWatcher : Component
+    {
+        public ParrotExtendedFileSystemWatcher()
+        {
+            watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.DirectoryName | NotifyFilters.Attributes | NotifyFilters.Size | NotifyFilters.LastWrite | NotifyFilters.LastAccess | NotifyFilters.CreationTime | NotifyFilters.Security;
+            watcher.Filter = "*.*";
+            watcher.IncludeSubdirectories = true;
+            watcher.Created += OnCreated;
+            watcher.Changed += OnChanged;
+            watcher.Deleted += OnDeleted;
+            watcher.Renamed += OnRenamed;
+            watcher.EnableRaisingEvents = false;
+            watcher.InternalBufferSize = 20480;
+        }
+
+        public event EventHandler ServiceStarted;
+
+        protected virtual void OnServiceStarted()
+        {
+            ServiceStarted?.Invoke(this, null);
+        }
+
+        public event EventHandler ServiceStopped;
+
+        protected virtual void OnServiceStopped(FileSystemEventArgs e)
+        {
+            ServiceStopped?.Invoke(this, null);
+        }
+
+        public event FileSystemEventHandler FileCreated;
+
+        protected virtual void OnFileCreated(FileSystemEventArgs e)
+        {
+            FileCreated?.Invoke(this, e);
+        }
+
+        public event FileSystemEventHandler FileDeleted;
+
+        protected virtual void OnFileDeleted(FileSystemEventArgs e)
+        {
+            FileDeleted?.Invoke(this, e);
+        }
+
+        public event FileSystemEventHandler FileChanged;
+
+        protected virtual void OnFileChanged(FileSystemEventArgs e)
+        {
+            FileChanged?.Invoke(this, e);
+        }
+
+        public event RenamedEventHandler FileRenamed;
+
+        protected virtual void OnFileRenamed(RenamedEventArgs e)
+        {
+            FileRenamed?.Invoke(this, e);
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("Choose when the watcher updates")]
+        public NotifyFilters UpdateOn
+        {
+            get => watcher.NotifyFilter;
+            set => watcher.NotifyFilter = value;
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("Watch subdirectories")]
+        public bool WatchSubdirectories
+        {
+            get => watcher.IncludeSubdirectories;
+            set => watcher.IncludeSubdirectories = value;
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("The path to watch")]
+        public string WatchPath
+        {
+            get => watcher.Path;
+            set
+            {
+                if (Directory.Exists(value))
+                {
+                    watcher.Path = value;
+                    return;
+                }
+                if (File.Exists(value))
+                {
+                    watcher.Path = Path.GetDirectoryName(value);
+                    watcher.Filter = Path.GetFileName(value);
+                }
+            }
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("Filter for certin files")]
+        public string Filters
+        {
+            get => watcher.Filter;
+            set => watcher.Filter = value;
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("The control to output to(via Text)")]
+        public Control OutputControl
+        {
+            get => outputControl;
+            set => outputControl = value;
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("Remove WatchPath from output")]
+        public bool SlimOutput
+        {
+            get => slimOutput;
+            set => slimOutput = value;
+        }
+
+        public void StartService()
+        {
+            ServiceStarted(this, null);
+            if (!watcher.EnableRaisingEvents)
+            {
+                watcher.EnableRaisingEvents = true;
+            }
+        }
+
+        public void StopService()
+        {
+            ServiceStopped(this, null);
+            if (watcher.EnableRaisingEvents)
+            {
+                watcher.EnableRaisingEvents = false;
+            }
+        }
+
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            DateTime lastWriteTime = File.GetLastWriteTime(e.FullPath);
+            if (lastWriteTime != lastRead && File.Exists(e.FullPath))
+            {
+                lastRead = lastWriteTime;
+                if (slimOutput)
+                {
+                    outputControl.Invoke(new SysAction(delegate ()
+                    {
+                        outputControl.Text = outputControl.Text + "\nChanged: " + e.FullPath.Replace(watcher.Path, "");
+                    }));
+                }
+                else
+                {
+                    outputControl.Invoke(new SysAction(delegate ()
+                    {
+                        outputControl.Text = outputControl.Text + "\nChanged: " + e.FullPath;
+                    }));
+                }
+                OnFileChanged(e);
+            }
+        }
+
+        private void OnDeleted(object source, FileSystemEventArgs e)
+        {
+            DateTime lastWriteTime = File.GetLastWriteTime(e.FullPath);
+            lastRead = lastWriteTime;
+            if (slimOutput)
+            {
+                if (outputControl.InvokeRequired)
+                {
+                    outputControl.Invoke(new SysAction(delegate ()
+                    {
+                        outputControl.Text = outputControl.Text + "\nDeleted: " + e.FullPath.Replace(watcher.Path, "");
+                    }));
+                }
+            }
+            else if (outputControl.InvokeRequired)
+            {
+                outputControl.Invoke(new SysAction(delegate ()
+                {
+                    outputControl.Text = outputControl.Text + "\nDeleted: " + e.FullPath;
+                }));
+            }
+            OnFileDeleted(e);
+        }
+
+        private void OnCreated(object source, FileSystemEventArgs e)
+        {
+            DateTime lastWriteTime = File.GetLastWriteTime(e.FullPath);
+            if (lastWriteTime != lastRead)
+            {
+                lastRead = lastWriteTime;
+                if (slimOutput)
+                {
+                    if (outputControl.InvokeRequired)
+                    {
+                        outputControl.Invoke(new SysAction(delegate ()
+                        {
+                            outputControl.Text = outputControl.Text + "\nCreated: " + e.FullPath.Replace(watcher.Path, "");
+                        }));
+                    }
+                }
+                else if (outputControl.InvokeRequired)
+                {
+                    outputControl.Invoke(new SysAction(delegate ()
+                    {
+                        outputControl.Text = outputControl.Text + "\nCreated: " + e.FullPath;
+                    }));
+                }
+                OnFileCreated(e);
+            }
+        }
+
+        public void OnRenamed(object source, RenamedEventArgs e)
+        {
+            DateTime lastWriteTime = File.GetLastWriteTime(e.FullPath);
+            if (lastWriteTime != lastRead)
+            {
+                lastRead = lastWriteTime;
+                if (slimOutput)
+                {
+                    if (outputControl.InvokeRequired)
+                    {
+                        outputControl.Invoke(new SysAction(delegate ()
+                        {
+                            outputControl.Text = outputControl.Text + "\nRenamed: " + e.FullPath.Replace(watcher.Path, "");
+                        }));
+                    }
+                }
+                else if (outputControl.InvokeRequired)
+                {
+                    outputControl.Invoke(new SysAction(delegate ()
+                    {
+                        outputControl.Text = outputControl.Text + "\nRenamed: " + e.FullPath;
+                    }));
+                }
+                OnFileRenamed(e);
+            }
+        }
+
+        private readonly FileSystemWatcher watcher = new();
+
+        private DateTime lastRead = DateTime.MinValue;
+
+        private Control outputControl;
+
+        private bool slimOutput = true;
+    }
+
+    #endregion
+}
