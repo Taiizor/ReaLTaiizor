4c7a091706e3356be17ad07a8d4afdbda225abb9
Taiizor <Taiizor@github.com>
2021-07-02 07:56:33 +0300

    53X Again 610!

---
+++ src/ReaLTaiizor/Controls/CheckBox/ParrotCheckBox.cs
@@ -0,0 +1,268 @@
+﻿#region Imports
+
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Windows.Forms;
+
+#endregion
+
+namespace ReaLTaiizor.Controls
+{
+    #region ParrotCheckBox
+
+    public class ParrotCheckBox : Control
+    {
+        public ParrotCheckBox()
+        {
+            base.Size = new Size(100, 20);
+            Text = base.Name;
+            ForeColor = Color.White;
+            currentColor = checkboxColor;
+            Cursor = Cursors.Hand;
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("Checked or unchecked")]
+        public bool Checked
+        {
+            get => isChecked;
+            set
+            {
+                isChecked = value;
+                OnCheckedStateChanged();
+                base.Invalidate();
+            }
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("Thickness of the tick when checked")]
+        public int TickThickness
+        {
+            get => tickThickness;
+            set
+            {
+                tickThickness = value;
+                base.Invalidate();
+            }
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("Checkbox color")]
+        public Color CheckboxColor
+        {
+            get => checkboxColor;
+            set
+            {
+                checkboxColor = value;
+                currentColor = value;
+                base.Invalidate();
+            }
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("Checkbox color")]
+        public Color CheckboxCheckColor
+        {
+            get => checkboxCheckColor;
+            set
+            {
+                checkboxCheckColor = value;
+                base.Invalidate();
+            }
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("Checkbox ios border color")]
+        public Color BorderColor
+        {
+            get => borderColor;
+            set
+            {
+                borderColor = value;
+                base.Invalidate();
+            }
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("Checkbox ios badge color")]
+        public Color BadgeColor
+        {
+            get => badgeColor;
+            set
+            {
+                badgeColor = value;
+                base.Invalidate();
+            }
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("Checkbox color when hovering")]
+        public Color CheckboxHoverColor
+        {
+            get => checkboxHoverColor;
+            set
+            {
+                checkboxHoverColor = value;
+                base.Invalidate();
+            }
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("The Checkbox style")]
+        public Style CheckboxStyle
+        {
+            get => checkboxStyle;
+            set
+            {
+                checkboxStyle = value;
+                base.Invalidate();
+            }
+        }
+
+        private SmoothingMode _SmoothingType = SmoothingMode.AntiAlias;
+        [Category("Parrot")]
+        [Browsable(true)]
+        public SmoothingMode SmoothingType
+        {
+            get { return _SmoothingType; }
+            set
+            {
+                _SmoothingType = value;
+                Invalidate();
+            }
+        }
+
+        private PixelOffsetMode _PixelOffsetType = PixelOffsetMode.HighQuality;
+        [Category("Parrot")]
+        [Browsable(true)]
+        public PixelOffsetMode PixelOffsetType
+        {
+            get { return _PixelOffsetType; }
+            set
+            {
+                _PixelOffsetType = value;
+                Invalidate();
+            }
+        }
+
+        private TextRenderingHint _TextRenderingType = TextRenderingHint.ClearTypeGridFit;
+        [Category("Parrot")]
+        [Browsable(true)]
+        public TextRenderingHint TextRenderingType
+        {
+            get { return _TextRenderingType; }
+            set
+            {
+                _TextRenderingType = value;
+                Invalidate();
+            }
+        }
+
+        public event EventHandler CheckedStateChanged;
+
+        protected virtual void OnCheckedStateChanged()
+        {
+            CheckedStateChanged?.Invoke(this, new EventArgs());
+        }
+
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            base.OnMouseEnter(e);
+            currentColor = checkboxHoverColor;
+            base.Invalidate();
+        }
+
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            base.OnMouseLeave(e);
+            currentColor = checkboxColor;
+            base.Invalidate();
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            base.OnClick(e);
+            if (!Checked)
+            {
+                Checked = true;
+                return;
+            }
+            Checked = false;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingType;
+            if (checkboxStyle == Style.Material)
+            {
+                e.Graphics.FillRectangle(new SolidBrush(currentColor), 1, 1, base.Height - 2, base.Height - 2);
+                if (isChecked)
+                {
+                    e.Graphics.DrawLine(new Pen(checkboxCheckColor, (float)tickThickness), 2, base.Height / 3 * 2, base.Height / 2, base.Height - 2);
+                    e.Graphics.DrawLine(new Pen(checkboxCheckColor, (float)tickThickness), base.Height / 2, base.Height - 2, base.Height - 2, 1);
+                }
+            }
+            if (checkboxStyle == Style.iOS)
+            {
+                if (!isChecked)
+                {
+                    e.Graphics.DrawEllipse(new Pen(BorderColor), 2, 2, base.Height - 4, base.Height - 4);
+                }
+                if (isChecked)
+                {
+                    e.Graphics.FillEllipse(new SolidBrush(BadgeColor), 1, 1, base.Height - 2, base.Height - 2);
+                    e.Graphics.DrawLine(new Pen(checkboxCheckColor, (float)tickThickness), base.Height / 5, base.Height / 2, base.Height / 2, base.Height / 4 * 3);
+                    e.Graphics.DrawLine(new Pen(checkboxCheckColor, (float)tickThickness), base.Height / 2, base.Height / 4 * 3, base.Height / 5 * 4, base.Height / 4);
+                }
+            }
+            StringFormat stringFormat = new()
+            {
+                LineAlignment = StringAlignment.Center,
+                Alignment = StringAlignment.Near
+            };
+            SolidBrush brush = new(ForeColor);
+            RectangleF layoutRectangle = new((float)(base.Height + 3), 0f, (float)(base.Width - base.Height - 2), (float)base.Height);
+            e.Graphics.PixelOffsetMode = PixelOffsetType;
+            e.Graphics.TextRenderingHint = TextRenderingType;
+            e.Graphics.DrawString(Text, Font, brush, layoutRectangle, stringFormat);
+            base.OnPaint(e);
+        }
+
+        private bool isChecked;
+
+        private int tickThickness = 2;
+
+        private Color checkboxColor = Color.FromArgb(0, 162, 250);
+
+        private Color checkboxCheckColor = Color.White;
+
+        private Color checkboxHoverColor = Color.FromArgb(249, 55, 98);
+
+        private Style checkboxStyle = Style.Material;
+
+        private Color currentColor;
+
+        private Color borderColor = Color.FromArgb(200, 200, 200);
+
+        private Color badgeColor = Color.FromArgb(0, 120, 255);
+
+        public enum Style
+        {
+            iOS,
+            Material
+        }
+    }
+
+    #endregion
+}
---
+++ src/ReaLTaiizor/Controls/PictureBox/ParrotPictureBox.cs
@@ -0,0 +1,284 @@
+﻿#region Imports
+
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Windows.Forms;
+
+#endregion
+
+namespace ReaLTaiizor.Controls
+{
+    #region ParrotPictureBox
+
+    public class ParrotPictureBox : Control
+    {
+        public ParrotPictureBox()
+        {
+            base.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            base.Size = new Size(150, 150);
+            base.SetStyle(ControlStyles.UserPaint | ControlStyles.ResizeRedraw | ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer, true);
+            x = 0 - base.Width / 2;
+            y = 0 - base.Height / 2;
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("Is the image eliptical")]
+        public bool IsElipse
+        {
+            get => isElipse;
+            set
+            {
+                isElipse = value;
+                base.Invalidate();
+            }
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("Is the image")]
+        public Image Image
+        {
+            get => image;
+            set
+            {
+                image = value;
+                base.Invalidate();
+            }
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("Is the image paralax zoom")]
+        public bool IsParallax
+        {
+            get => isParallax;
+            set
+            {
+                isParallax = value;
+                base.Invalidate();
+            }
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("Enable filters")]
+        public bool FilterEnabled
+        {
+            get => filterEnabled;
+            set
+            {
+                filterEnabled = value;
+                base.Invalidate();
+            }
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("Filter color left")]
+        public Color ColorLeft
+        {
+            get => colorLeft;
+            set
+            {
+                colorLeft = value;
+                base.Invalidate();
+            }
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("Filter color right")]
+        public Color ColorRight
+        {
+            get => colorRight;
+            set
+            {
+                colorRight = value;
+                base.Invalidate();
+            }
+        }
+
+        [Category("Parrot")]
+        [Browsable(true)]
+        [Description("Filter alpha")]
+        public int FilterAlpha
+        {
+            get => filterAlpha;
+            set
+            {
+                filterAlpha = value;
+                base.Invalidate();
+            }
+        }
+
+        private SmoothingMode _SmoothingType = SmoothingMode.AntiAlias;
+        [Category("Parrot")]
+        [Browsable(true)]
+        public SmoothingMode SmoothingType
+        {
+            get => _SmoothingType;
+            set
+            {
+                _SmoothingType = value;
+                Invalidate();
+            }
+        }
+
+        private PixelOffsetMode _PixelOffsetType = PixelOffsetMode.HighQuality;
+        [Category("Parrot")]
+        [Browsable(true)]
+        public PixelOffsetMode PixelOffsetType
+        {
+            get => _PixelOffsetType;
+            set
+            {
+                _PixelOffsetType = value;
+                Invalidate();
+            }
+        }
+
+        private CompositingQuality _CompositingQualityType = CompositingQuality.HighQuality;
+        [Category("Parrot")]
+        [Browsable(true)]
+        public CompositingQuality CompositingQualityType
+        {
+            get => _CompositingQualityType;
+            set
+            {
+                _CompositingQualityType = value;
+                Invalidate();
+            }
+        }
+
+        private TextRenderingHint _TextRenderingType = TextRenderingHint.ClearTypeGridFit;
+        [Category("Parrot")]
+        [Browsable(true)]
+        public TextRenderingHint TextRenderingType
+        {
+            get => _TextRenderingType;
+            set
+            {
+                _TextRenderingType = value;
+                Invalidate();
+            }
+        }
+
+        private InterpolationMode _InterpolationType = InterpolationMode.HighQualityBilinear;
+        [Category("Parrot")]
+        [Browsable(true)]
+        public InterpolationMode InterpolationType
+        {
+            get => _InterpolationType;
+            set
+            {
+                _InterpolationType = value;
+                Invalidate();
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            BufferedGraphicsContext bufferedGraphicsContext = BufferedGraphicsManager.Current;
+            bufferedGraphicsContext.MaximumBuffer = new Size(base.Width, base.Height);
+            bufferedGraphics = bufferedGraphicsContext.Allocate(base.CreateGraphics(), base.ClientRectangle);
+            bufferedGraphics.Graphics.SmoothingMode = SmoothingType;
+            bufferedGraphics.Graphics.InterpolationMode = InterpolationType;
+            bufferedGraphics.Graphics.CompositingQuality = CompositingQualityType;
+            bufferedGraphics.Graphics.PixelOffsetMode = PixelOffsetType;
+            bufferedGraphics.Graphics.TextRenderingHint = TextRenderingType;
+            bufferedGraphics.Graphics.Clear(BackColor);
+            e.Graphics.SmoothingMode = SmoothingType;
+            e.Graphics.InterpolationMode = InterpolationType;
+            e.Graphics.CompositingQuality = CompositingQualityType;
+            e.Graphics.PixelOffsetMode = PixelOffsetType;
+            if (image != null)
+            {
+                if (!isParallax)
+                {
+                    if (isElipse)
+                    {
+                        Brush brush = new TextureBrush(new Bitmap(image, base.Width, base.Height), new Rectangle(0, 0, base.Width, base.Height));
+                        e.Graphics.FillEllipse(brush, 0, 0, base.Width, base.Height);
+                        if (filterEnabled)
+                        {
+                            Brush brush2 = new LinearGradientBrush(base.ClientRectangle, Color.FromArgb(filterAlpha, colorRight), Color.FromArgb(filterAlpha, colorLeft), 180f);
+                            e.Graphics.FillEllipse(brush2, 0, 0, base.Width, base.Height);
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        e.Graphics.DrawImage(new Bitmap(image, base.Width, base.Height), 0, 0);
+                        if (filterEnabled)
+                        {
+                            Brush brush3 = new LinearGradientBrush(base.ClientRectangle, Color.FromArgb(filterAlpha, colorRight), Color.FromArgb(filterAlpha, colorLeft), 180f);
+                            e.Graphics.FillRectangle(brush3, 0, 0, base.Width, base.Height);
+                            return;
+                        }
+                    }
+                }
+                else if (isParallax)
+                {
+                    try
+                    {
+                        bufferedGraphics.Graphics.DrawImage(new Bitmap(image, base.Width * 2, base.Height * 2), x, y);
+                        bufferedGraphics.Render(e.Graphics);
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+        }
+
+        private void updateParallax()
+        {
+            try
+            {
+                bufferedGraphics.Graphics.Clear(BackColor);
+                bufferedGraphics.Graphics.DrawImage(new Bitmap(image, base.Width * 2, base.Height * 2), x, y);
+                bufferedGraphics.Render(base.CreateGraphics());
+            }
+            catch
+            {
+            }
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if (isParallax)
+            {
+                x = e.X - base.Width;
+                y = e.Y - base.Height;
+                base.Invalidate();
+            }
+        }
+
+        private int x;
+
+        private int y;
+
+        private BufferedGraphics bufferedGraphics;
+
+        private bool isElipse;
+
+        private Image image;
+
+        private bool isParallax;
+
+        private bool filterEnabled = true;
+
+        private Color colorLeft = Color.DodgerBlue;
+
+        private Color colorRight = Color.DodgerBlue;
+
+        private int filterAlpha = 200;
+    }
+
+    #endregion
+}
