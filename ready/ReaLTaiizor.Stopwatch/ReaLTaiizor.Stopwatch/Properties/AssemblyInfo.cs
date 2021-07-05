100aac67bcc927a2e629ef6d963afb869e358282
Taiizor <Taiizor@github.com>
2021-07-05 20:27:44 +0300

    Ready Preparation
    Ready stopwatch preparation

---
+++ ready/ReaLTaiizor.Stopwatch/ReaLTaiizor.Stopwatch/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("ReaLTaiizor Stopwatch")]
+[assembly: AssemblyDescription("ReaLTaiizor Stopwatch v1.0.0.0")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Taiizor")]
+[assembly: AssemblyProduct("ReaLTaiizor Stopwatch")]
+[assembly: AssemblyCopyright("Copyright © 2021 Taiizor")]
+[assembly: AssemblyTrademark("ReaLTaiizor")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("fae04ec0-301f-11d3-bf4b-00c04f79efbc")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ready/ReaLTaiizor.Stopwatch/ReaLTaiizor.Stopwatch/Stopwatch.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace ReaLTaiizor.Stopwatch
+{
+    public partial class Stopwatch : Form
+    {
+        public Stopwatch()
+        {
+            InitializeComponent();
+        }
+    }
+}
