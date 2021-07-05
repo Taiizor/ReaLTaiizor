4a31e644ca508f4e4eddc20cad2c5d84d6ed9278
Taiizor <Taiizor@github.com>
2021-07-05 15:56:53 +0300

    Tutorial Added
    Summon form

---
+++ tutorial/C#/#002 form-summon/form-summon/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Windows.Forms;
+
+namespace form_summon
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Crown());
+        }
+    }
+}
---
+++ tutorial/C#/#002 form-summon/form-summon/Royal.cs
@@ -0,0 +1,12 @@
+﻿using ReaLTaiizor.Forms;
+
+namespace form_summon
+{
+    public partial class Royal : RoyalForm
+    {
+        public Royal()
+        {
+            InitializeComponent();
+        }
+    }
+}
