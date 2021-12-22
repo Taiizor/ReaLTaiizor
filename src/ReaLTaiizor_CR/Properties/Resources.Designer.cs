dd7e3111b320c4ceece6cfe91c61ba23f0c09ba5
Taiizor <taiizor@soferity.com>
2021-12-22 17:25:36 +0300

    Version Update

--- src/ReaLTaiizor_CR/Properties/Resources.Designer.cs
+++ src/ReaLTaiizor_CR/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace ReaLTaiizor_CR.Properties {
     // sınıfı tarafından otomatik olarak oluşturuldu.
     // Üye eklemek veya kaldırmak için .ResX dosyanızı düzenleyin ve sonra da ResGen
     // komutunu /str seçeneğiyle yeniden çalıştırın veya VS projenizi yeniden oluşturun.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
