fbac257fc1954f9ac65d54e7b627e5b8a3fe3f06
Taiizor <41683699+Taiizor@users.noreply.github.com>
2022-07-16 00:01:29 +0300

    Revise

--- src/ReaLTaiizor/ReaLTaiizor.cs
+++ src/ReaLTaiizor/ReaLTaiizor.cs
@@ -13,7 +13,7 @@ using System.Windows.Forms;
 //     Creator: Taiizor
 //     Website: www.Vegalya.com
 //     Created: 15.May.2019
-//     Changed: 14.Jul.2022
+//     Changed: 16.Jul.2022
 //     Version: 3.7.8.7
 //
 // |---------DO-NOT-REMOVE---------|
