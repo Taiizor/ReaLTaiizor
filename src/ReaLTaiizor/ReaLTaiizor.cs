4a2ec5a2b8f38a5ca492f8474df339b872ffcefe
Taiizor <taiizor@vegalya.com>
2023-04-11 22:48:38 +0300

    Update SDK

--- src/ReaLTaiizor/ReaLTaiizor.cs
+++ src/ReaLTaiizor/ReaLTaiizor.cs
@@ -13,7 +13,7 @@ using System.Windows.Forms;
 //     Creator: Taiizor
 //     Website: www.Vegalya.com
 //     Created: 15.May.2019
-//     Changed: 29.Mar.2023
+//     Changed: 11.Apr.2023
 //     Version: 3.7.9.3
 //
 // |---------DO-NOT-REMOVE---------|
