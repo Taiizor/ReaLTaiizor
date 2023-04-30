43e5f8a071ee05b018e9f7bdc6c43c059196a3f7
Taiizor <41683699+Taiizor@users.noreply.github.com>
2023-04-30 22:48:52 +0300

    Updated Instagram Sample

--- src/ReaLTaiizor/ReaLTaiizor.cs
+++ src/ReaLTaiizor/ReaLTaiizor.cs
@@ -13,7 +13,7 @@ using System.Windows.Forms;
 //     Creator: Taiizor
 //     Website: www.Vegalya.com
 //     Created: 15.May.2019
-//     Changed: 20.Apr.2023
+//     Changed: 30.Apr.2023
 //     Version: 3.7.9.3
 //
 // |---------DO-NOT-REMOVE---------|
