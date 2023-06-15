50dfe6f8c03d88345aa49db01437e50e565265b9
Taiizor <41683699+Taiizor@users.noreply.github.com>
2023-06-15 19:58:53 +0300

    Revise MP3 PlayerSample

--- sample/ReaLTaiizor.Player/ReaLTaiizor.Player/Properties/Resources.Designer.cs
+++ sample/ReaLTaiizor.Player/ReaLTaiizor.Player/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace ReaLTaiizor.Player.Properties {
     // sınıfı tarafından otomatik olarak oluşturuldu.
     // Üye eklemek veya kaldırmak için .ResX dosyanızı düzenleyin ve sonra da ResGen
     // komutunu /str seçeneğiyle yeniden çalıştırın veya VS projenizi yeniden oluşturun.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -106,6 +106,16 @@ namespace ReaLTaiizor.Player.Properties {
         internal static System.Drawing.Bitmap available_updates_64px {
             get {
                 object obj = ResourceManager.GetObject("available_updates_64px", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap türünde yerelleştirilmiş bir kaynak arar.
+        /// </summary>
+        internal static System.Drawing.Bitmap Background {
+            get {
+                object obj = ResourceManager.GetObject("Background", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
--- src/ReaLTaiizor/ReaLTaiizor.cs
+++ src/ReaLTaiizor/ReaLTaiizor.cs
@@ -13,7 +13,7 @@ using System.Windows.Forms;
 //     Creator: Taiizor
 //     Website: www.Vegalya.com
 //     Created: 15.May.2019
-//     Changed: 14.Jun.2023
+//     Changed: 15.Jun.2023
 //     Version: 3.7.9.4
 //
 // |---------DO-NOT-REMOVE---------|
