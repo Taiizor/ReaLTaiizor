22ee7d588fd2f820e59fe3e404b7f9b89f4c3e6e
Taiizor <41683699+Taiizor@users.noreply.github.com>
2023-08-09 14:16:52 +0300

    Update Packages

--- src/ReaLTaiizor/ReaLTaiizor.cs
+++ src/ReaLTaiizor/ReaLTaiizor.cs
@@ -13,7 +13,7 @@ using System.Windows.Forms;
 //     Creator: Taiizor
 //     Website: www.Vegalya.com
 //     Created: 15.May.2019
-//     Changed: 05.Aug.2023
+//     Changed: 09.Aug.2023
 //     Version: 3.7.9.5
 //
 // |---------DO-NOT-REMOVE---------|
